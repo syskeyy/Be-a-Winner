8b0316a63924082c00ce634589d58ebfc93ed777
syskeyy <dgulijev@gmail.com>
2024-03-06 13:56:26 +0000

    added some prize details in index, I am not happy with this, I will change this later

--- with-passport-app/pages/index.js
+++ with-passport-app/pages/index.js
@@ -8,6 +8,16 @@ const Home = () => {
     <Layout>
       <h1>🎉 BE A WINNER! 🎉</h1>
       <p>Be a winner today by entering in the raffle and win awesome prizes! </p>
+      <div className={styles.indexContainer}>
+        <div className={styles.raffleDetails}>
+          <h2>Raffle Grand Prize!</h2>
+          <img src="holiday-img.jpg" alt="Raffle Prize Image"/>
+          <p>This is a description of the raffle prize. It could be a car, a vacation, or any other exciting prize.</p>
+          <h3>Draw Date</h3>
+          <p>December 31, 2024.</p>
+          <button>Enter Raffle</button>
+        </div>
+      </div>
     </Layout>
   );
 };
