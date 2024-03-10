45eaa18f9244b18422bc2bd3b2c8f9127d83c231
syskeyy <dgulijev@gmail.com>
2024-03-10 22:42:00 +0000

    Nested google font inside head tags, maybe it will now show poppins on linux

--- Be-A-Winner/pages/_app.js
+++ Be-A-Winner/pages/_app.js
@@ -1,9 +1,15 @@
 import '../styles/global.css';
-<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"></link>
+import Head from 'next/head';
 
-//https://www.youtube.com/watch?v=Bd_CorhPq_M
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-  }
-  
-  export default MyApp;
+  return (
+    <>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
